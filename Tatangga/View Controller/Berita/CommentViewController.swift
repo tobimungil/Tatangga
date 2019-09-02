1ebd7db6784747d75ef5fe6079579156c9f6c129
Mirza Fachreza <mirzaf99@gmail.com>
2019-09-02 15:54:51 +0700

    lanjut

---
+++ Tatangga/View Controller/Berita/CommentViewController.swift
@@ -0,0 +1,39 @@
+//
+//  CommentViewController.swift
+//  Tatangga
+//
+//  Created by Mirza Fachreza 2 on 02/09/19.
+//  Copyright © 2019 Mirza Fachreza. All rights reserved.
+//
+
+import UIKit
+
+class CommentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
